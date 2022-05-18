76941a5d15ae912b48751f023e61b3fa1d851cba
InfamyStudio <admin@infamystudio.com>
2022-05-18 23:06:05 +0100

    Updated program to not require to be opened again after every new search

--- VRising DedicatedServer Text Scraper.py
+++ VRising DedicatedServer Text Scraper.py
@@ -5,25 +5,32 @@ print("Please Make Sure To Change Your Path To Your Server Folder In The Code - 
 
 path = "E:/SteamLibrary/steamapps/common/VRisingDedicatedServer/"
 filelist = []
+while True:
+    query = str(input("Enter the text you want to search for: "))
 
-query = str(input("Enter the text you want to search for: "))
+    for root, dirs, files in os.walk(path):
+        for file in files:
+            if file.endswith(".txt") or file.endswith(".log") or file.endswith(".json") or file.endswith(".info"):
+                filelist.append(os.path.join(root,file))
 
-for root, dirs, files in os.walk(path):
-    for file in files:
-        if file.endswith(".txt") or file.endswith(".log") or file.endswith(".json") or file.endswith(".info"):
-            filelist.append(os.path.join(root,file))
+    results = False
+    for name in filelist:
+        f = open(name, "r", encoding="utf8")
+        contents = f.readlines()
+        for line in contents:
+            if query in line:
+                results = True
+                print("In File: " + name + "\nFull Line String Was Mentioned: " + line + "Line Number: " + str(contents.index(line)))
 
-results = False
-for name in filelist:
-    f = open(name, "r", encoding="utf8")
-    contents = f.readlines()
-    for line in contents:
-        if query in line:
-            results = True
-            print("In File: " + name + "\nFull Line String Was Mentioned: " + line + "Line Number: " + str(contents.index(line)))
+    if results == False:
+        print("No Results With Search Query!")
+    #ask if they want to search again
+    again = str(input("Would you like to search again? (y/n): ")).upper()
+    if again == "Y":
+        print("Restarting Search!")
+    if again == "N":
+        print("Exiting Search!")
+        f.close()
+        break
 
-if results == False:
-    print("No Results With Search Query!")
 
-input("Press Enter To Exit...")
-f.close()
